7f6e30f19e9e636d77c1c9fbcc454de2dbe21ffe
Aidan Loughran <aidlo.dev@pm.me>
2022-06-20 17:43:05 +0000

    Check internet connectivity

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+ONLINE=0
+
 # Load keyboard layout
 while read -p "loadkeys(uk): " input; do
     if [ "$input" == "" ]; then input=uk; fi
     loadkeys "$input" &> /dev/null
-    if [ "$?" == "0" ]; then break; fi
+    if [ $? -eq 0 ]; then break; fi
     echo "Not recognised.";
 done
+
+# Check for internet connectivity
+if : >/dev/tcp/8.8.8.8/53; then
+    ONLINE=1
+    echo "Internet available."
+else
+    echo "Internet unavailable."
+fi
 
